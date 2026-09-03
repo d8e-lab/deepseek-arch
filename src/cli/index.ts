71e5591749c44adc2cdcafe1e2e819961d7dd6f1
helck <bcsun@stu.xmu.edu.cn>
2026-09-04 00:39:41 +0800

    chore: bump version to 1.5.2

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -38,7 +38,7 @@ function loadMasterTools(debug = false, selfInteraction = false) {
 	return tools;
 }
 
-const PACKAGE_VERSION = "1.5.1";
+const PACKAGE_VERSION = "1.5.2";
 
 async function createTuiConfig(): Promise<TuiConfig> {
 	const cfg = await ConfigManager.getInstance().load();
