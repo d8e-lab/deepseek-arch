d8df68a4a9d267aa50896962a06af0f9d0cb92c6
helck <bcsun@stu.xmu.edu.cn>
2026-09-18 19:49:31 +0800

    chore: bump version to 2.0.1

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -43,7 +43,7 @@ function loadMasterTools(debug = false, selfInteraction = false) {
 	return tools;
 }
 
-const PACKAGE_VERSION = "2.0.0";
+const PACKAGE_VERSION = "2.0.1";
 
 async function createTuiConfig(): Promise<TuiConfig> {
 	const cfg = await ConfigManager.getInstance().load();
